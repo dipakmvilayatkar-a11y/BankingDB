23f62f5b4476de2e0466caf6b7f4111cf0d5cd5d
dipakmvilayatkar-a11y <dipakmvilayatkar@gmail.com>
2026-07-28 10:07:05 +0530

    create bank data

---
+++ BankingDB.sql
@@ -0,0 +1,15 @@
+-- show databases
+CREATE DATABASE BankingDB;
+USE bankingdb;
+create table Customers (
+CustomerID int primary key,
+FirstName varchar(50),
+LastName varchar(50),
+Email varchar(100),
+Phone varchar(15),
+AccountcreationDate date
+);
+
+describe customers;
+
+select * from customers;
