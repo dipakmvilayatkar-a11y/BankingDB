46b69ff54a352f9f5b72d5045b0fe8accdd1d842
dipakmvilayatkar-a11y <dipakmvilayatkar@gmail.com>
2026-07-29 09:18:00 +0530

    created new tables transactions,loans and others

--- BankingDB.sql
+++ BankingDB.sql
@@ -14,3 +14,37 @@ describe customers;
 
 select * from customers;
 
+
+create table Accounts (
+AccountID int,
+AccountType varchar(20),
+Balance decimal(10,2)
+);
+describe Accounts;
+
+create table Transactions (
+TransactionID int,
+TransactionDate date,
+Amount decimal(10,2),
+TransactionType varchar(20)
+);
+describe Transactions;
+
+create table Branches (
+BranchID int,
+BranchName varchar(100),
+BranchAddress varchar(200),
+BranchPhone varchar(15)
+);
+desc Branches;
+
+create table AccountBranches (
+AssignmentDate date
+);
+create table Loans (
+LoanID int,
+LoanAmount decimal(10,2),
+InterestRate decimal(5,2),
+StartDate date,
+EndDate date
+);
