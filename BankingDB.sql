6dbdf4496bad5f9208bb2a2e26338abfb70541e5
dipakmvilayatkar-a11y <dipakmvilayatkar@gmail.com>
2026-08-07 09:17:29 +0530

    limit,offset,case

--- BankingDB.sql
+++ BankingDB.sql
@@ -205,6 +205,8 @@ Values
 (403,'2026-08-04','2500','NB','203'),
 (404,'2026-08-04','7000','UPI','204');
 
+
+
 SELECT * from transactions;
 
 insert into customers values (105,'karan','mehta','karan@gmail.com','7709540869','2026-07-25','1998-10-29');
@@ -259,3 +261,171 @@ FROM
     
     select * from accounts
     where not AccountType = 'saving';
+    
+    -- find all customers register after 1 july 2026
+    
+    select * from customers
+    where AccountcreationDate > '2026-07-01';
+    
+    -- Query multiple customers using IN operator (use phone number)
+    
+    select FirstName,LastName,Phone from customers
+    where Phone in (9870123211,7709540869);
+    
+     select FirstName,LastName,Phone from customers 
+    where Phone = '9518562312' or Phone = '9923456799' or Phone = '9870123211';
+    
+    -- BETWEEN operator
+    -- find customers having balance between 10000 to 30000
+    SELECT 
+    CustomerID, AccountType, Balance
+FROM
+    accounts
+WHERE
+    Balance BETWEEN 10000 AND 30000;
+    -- the values specified in range are included in the results.
+    
+   SELECT 
+    CustomerID, AccountType, Balance
+FROM
+    accounts
+WHERE
+    Balance >= 10000 AND Balance <= 30000;  
+    
+    -- like operator used for pattern matching.
+   -- find all customers whose first name starts with letter "K" 
+   -- "_" matches exactly one character.
+
+select * from customers where firstName like 'K%'; 
+
+-- find all customers whose last name ends with letter "A" 
+
+select * from customers where LastName like '%A'; 
+
+-- find all customers whose last name has exactly 5 characters;
+Select * from customers
+where LastName Like '_____' ;
+
+-- ORDER BY Clause
+-- sort the accounts table according to the customers balance.
+
+SELECT 
+    CustomerID, Balance
+FROM
+    accounts
+ORDER BY Balance;
+
+-- sort  the branches table according to the branchname.
+SELECT 
+    BranchID, BranchName
+FROM
+    branches
+ORDER BY BranchName;
+
+-- sort the accounts table according to the customers balance from highest to lowest amount.
+
+SELECT 
+    CustomerID, Balance
+FROM
+    accounts
+ORDER BY Balance DESC;
+
+-- sort according to multiple columns
+-- sort account table according to the accounttype and balance
+SELECT 
+    AccountID,AccountType,Balance,CustomerID
+FROM
+    accounts
+    
+ORDER BY AccountType desc,Balance desc;
+
+-- DISTINCT clause.
+-- find distinct(unique) accounttype from accounts table
+
+select distinct accounttype from accounts;
+-- find distinct(unique) transaction type and account id from transaction  table
+select distinct transactiontype,accountid from transactions;
+
+select * from transactions
+limit 2; -- use of limit clause
+
+-- offset clause is used to skip specific row before return
+
+select * from customers
+limit 2 offset 2;
+
+select * from accounts
+order by Balance desc
+limit 2;
+
+select * from accounts
+order by Balance desc
+limit 1 offset 2;
+
+select * from accounts
+order by Balance desc
+limit 2,1;   -- here 2 specifies the rows to skip and 1 specifies the number of rows to return
+
+Insert into customers
+(CustomerID,FirstName,LastName,Email,Phone,AccountCreationDate,DateofBirth)
+values
+(106,'Prasad','Patil','Prasad@gmail.com',null,'2026-02-15','1995-11-16'),
+(107,'Sachin','Gedam','Gedam@gmail.com','9665623649','2026-07-28','1996-09-25'),
+(108,'Sandhya','Alone','Sandhya@gmail.com',null,'2026-01-02','1997-10-28');
+
+-- null and not null values
+select * from customers
+where Phone is null;
+
+select * from customers
+where Phone is not null;
+
+insert into accounts
+(AccountID,CustomerID,AccountType,Balance)
+Values
+(205,'105','Current','50000'),
+(206,'106','Savings','75000'),
+(207,'107','Current','29000'),
+(208,'108','Savings','19000');
+
+insert into transactions
+(TransactionID,TransactionDate,Amount,TransactionType,AccountID)
+Values
+(405,'2026-08-05','16000','deposit','205'),
+(406,'2026-08-05','450','withdrawl','206'),
+(407,'2026-08-06','25000','withdrawl','207'),
+(408,'2026-08-07','5000','deposit','208');
+
+insert into loans
+(LoanID,LoanAmount,InterestRate,StartDate,EndDate,CustomerID)
+Values
+(305,'2500000','9.5','2021-09-28','2026-09-28','105'),
+(308,'700000','11.5','2022-08-06','2027-08-06','108');
+
+select * from transactions;
+select * from loans;
+select * from customers;
+select * from accounts;
+
+UPDATE accounts
+SET BranchID = '002'
+WHERE AccountID = 205;
+
+UPDATE accounts
+SET BranchID = '001'
+WHERE AccountID = 206;
+
+UPDATE accounts
+SET BranchID = '003'
+WHERE AccountID = 207;
+
+UPDATE accounts
+SET BranchID = '002'
+WHERE AccountID = 208;
+
+select AccountID,Accounttype,Balance,
+case
+when balance >= 50000 then "High Value Customer"
+else "Low Value Customer"
+end as CustomerCategory
+from accounts;
